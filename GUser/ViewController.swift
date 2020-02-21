043f05b84672be74e11ad6f2e0c99ca3be741270
Wei Kuo <jtes5401@gmail.com>
2020-02-21 12:24:15 +0800

    Initial Commit

---
+++ GUser/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GUser
+//
+//  Created by Wei Kuo on 2020/2/21.
+//  Copyright © 2020 Wei Kuo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
