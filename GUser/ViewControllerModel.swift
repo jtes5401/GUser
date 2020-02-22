bdccf43f7ee7dda9359512debafb2c0854513447
Wei Kuo <jtes5401@gmail.com>
2020-02-22 17:07:12 +0800

    Add ViewModel frame for ViewController

---
+++ GUser/ViewControllerModel.swift
@@ -0,0 +1,13 @@
+//
+//  ViewControllerModel.swift
+//  GUser
+//
+//  Created by Wei Kuo on 2020/2/22.
+//  Copyright © 2020 Wei Kuo. All rights reserved.
+//
+
+import Foundation
+
+class ViewControllerModel {
+    
+}
