caba2c0983c1da560c9aba3b9435bceca10e981b
Wei Kuo <jtes5401@gmail.com>
2020-02-22 14:11:44 +0800

    Add new api for Users
    This api can get users from github with size and begin.

--- GUser/GHConnecter.swift
+++ GUser/GHConnecter.swift
@@ -14,6 +14,17 @@ class GHConnecter: NSObject {
     let session = URLSession.shared
     let decoder = JSONDecoder()
     
+    public func requestUsers(since: Int, size: Int = 20, callBack:@escaping (_ users:[GHUser]?, _ result:Bool)->()) {
+        let s = size == 0 ? 20 : size
+        let path = host + "/users?since=\(since)&per_page=\(s)"
+        
+        guard let url = URL(string: path) else {
+            callBack(nil,false)
+            return
+        }
+        let request = URLRequest(url:url)
+        self.sendRequest(request: request, type: [GHUser].self, callBack: callBack)
+    }
     
     private func sendRequest<T:Decodable>(request: URLRequest, type: T.Type,callBack:@escaping ((T?, Bool)->Void)) {
         session.dataTask(with: request) { (data, response, err) in
